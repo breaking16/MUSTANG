5adaad2e6a7e998bfa30f21b7fc98d909dffdbb4
breaking16 <w.w804.162@gmail.com>
2024-01-08 23:33:36 +0200

    4half A4party

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ $(function () {
     arrows: false,
     dots: true,
     slidesToShow: 3,
+   responsive: [
+    {
+      breakpoint: 841,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+     {
+      breakpoint: 601,
+      settings: {
+        slidesToShow: 1
+      }
+    },
+  ]
   });
 });
